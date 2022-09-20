dc4fc4807224da0fa1390900ce81d4e611c67d68
alayala2025 <alayala@hartford.edu>
2022-09-20 17:28:23 -0400

    Print out initials name

--- Assignment1.java
+++ Assignment1.java
@@ -3,6 +3,13 @@
 public class Assignment1 {
     public static void main(String[] args) {
         // Add your code here
-        System.out.print(letter1)
+        System.out.print("AAAAAAAAA   JJJJJJJJJ");
+        System.out.print("AAA   AAA   JJJJJJJJJ");
+        System.out.print("AAA   AAA      JJJ   ");
+        System.out.print("AAAAAAAAA      JJJ   ");
+        System.out.print("AAAAAAAAA      JJJ   ");
+        System.out.print("AAAAAAAAA      JJJ   ");
+        System.out.print("AAA   AAA   JJ JJJ    ");
+        System.out.print("AAA   AAA    JJJ     ");
     }
 }
